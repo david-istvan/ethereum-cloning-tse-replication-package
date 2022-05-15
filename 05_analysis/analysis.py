3508b05a1e2400bf517c81ddf246f231cfbe62a6
eff-kay <faizan10114@gmail.com>
2022-05-14 22:17:25 -0400

    rq2 updated observation 12, 13, 14 updated gitignore updated

--- 05_analysis/analysis.py
+++ 05_analysis/analysis.py
@@ -1,7 +1,11 @@
 import argparse
 import os
+from pprint import pformat
 import shutil
 from statistics import mean, median, stdev
+import re
+import pickle
+from typing import Collection
 
 import numpy as np
 import pandas as pd
@@ -380,9 +384,91 @@ class Analysis():
         #Same as observation4()
         
     def observation6(self):
+        print('inside observation 6')
+
+        def get_classids(file_path)->int:
+            a = open(file_path).read()
+            b = re.findall('.*?classid="(\d+)".*', a)
+
+            return b
+
+        def extract_code(file_path, classids, save_path):
+            f  = open(file_path).read()
+            
+            save_list = []
+            for i in map(str, classids):
+                p = r'^(<class classid="{}"[\s\S]*?<\/class>)$'.format(i)
+                cs = re.findall(p, f, re.MULTILINE)
+                p = r'^<source[\s\S]*?function[ ]?([\s\S]*?)\([\s\S]*<\/source>$'
+                all_fs = re.findall(p, cs[0], re.MULTILINE)
+                if len(all_fs)==0:
+                    p = r'^<source[\s\S]*?>([\s\S]*?)<\/source>$'
+                    cs = re.findall(p, cs[0], re.MULTILINE)
+                    p = r'[\s\S]*?function[ ]?([\s\S]*?)\([\s\S]*?$'
+                    all_fs = re.findall(p, cs[0], re.MULTILINE)
+                    if len(all_fs)==0:
+                        print('still not caught', cs[0])
+
+                save_list.append(all_fs)
+                print(f'class id {i} done')
+            pickle.dump(save_list, open(save_path, 'wb'))
+
+        def extract_functions_ids(config):
+            original_paths = ['type-1', 'type-2', 'type-2c', 'type-3-1', 'type-3-2', 'type-3-2c']
+            print('extr')
+            os.makedirs('duplicates/code-filtered', exist_ok=True)
+            os.makedirs('duplicates/function-ids', exist_ok=True)
+            for filepath in original_paths:
+                print(f'starting {filepath}')
+                classids = get_classids('duplicates/final/'+filepath+".xml")
+                print(f'classsids {len(classids)}')
+                complete_file_path = 'data/{}/withsource/{}'.format(config, filepath+'.xml')
+                save_path_pickle = 'duplicates/function-ids/{}'.format(filepath+'.p')
+                extract_code(complete_file_path, classids, save_path_pickle)
+            
+                save_path_txt = open('duplicates/function-ids/{}'.format(filepath+'.txt'), 'w')
+                functions = pickle.load(open(save_path_pickle, 'rb'))
+
+                for f in functions:
+                    save_path_txt.write(f[0]+'\n') 
+
+                print(len(classids), len(functions))
+
+        def get_top_function_ids(path):
+            original_paths = ['type-1', 'type-2', 'type-2c', 'type-3-1', 'type-3-2', 'type-3-2c']
+
+            save_path = open(f'{path}/function-ids/all-ids.txt', 'w')
+            for file in original_paths:
+                # ids = file.read().split('\n')
+                file_data = open(f'{path}/function-ids/{file}.txt').read()
+                # print(f'{file}', file_data)
+                save_path.write(file_data)
+            save_path.flush()
+
+            top20_functions = pd.Series(open(f'{path}/function-ids/all-ids.txt', 'r').read().split('\n')[:-1]).value_counts()[:20]
+
+            print(top20_functions.to_latex())
+            top20_path = open(f'{path}/function-ids/top20.txt', 'w')
+            
+            [top20_path.write(f'{x}\n') for x in top20_functions.index]
+            return top20_functions
+
+        try:
+            os.chdir('data')
+        except FileNotFoundError:
+            raise FileNotFoundError("You need to place data folder at the same location as analysis.py")
+        # use this folder for testing purposes
+        # os.chdir('observation6-test-data')
+
+        extract_functions_ids('macro')
+        get_top_function_ids('duplicates')
+
+        # go back to parent dir
+        os.chdir('../')
         pass
 
     def observation7(self):
+        # Same as observation 6
         pass
     
     def observation8(self):
@@ -651,11 +737,37 @@ class Analysis():
             'Percentage ratio: {}%.'.format(round((len(uniqueContractsWithIdenticalOZBlock)/len(unique_contracts))*100, 2))
         ]
         
-        self.printTextReport(12, report)
+        self.printTextReport(122, report)
+
+    def observation12b(self):
+        df = pd.read_csv('../03_clones/rq3/type-1_functions.csv')
+        df['endline_x'] = df['endline_x'].apply( lambda x: int(x.replace('"', '')))
+        df['startline_x'] = df['startline_x'].apply( lambda x: int(x.replace('"', '')))
+        df['diff'] = df['endline_x'] - df['startline_x'] + 1
+
+        reports = []
+            
+        for i in range(11):
+            df = df[df['diff'] >= i]
+
+            uniqueContractsWithIdenticalOZBlock = (df[df['startline_y'].notnull()].drop_duplicates(subset=['filename_x']))
+            unique_contracts = df.drop_duplicates(subset=['filename_x'])
+            
+            report = (
+                f'Min number of function lines considered: {i}',
+                'Total number of contracts: {}.'.format(len(unique_contracts)),
+                'Number of distinct contracts with an OpenZeppelin record associated: {}.'.format(len(uniqueContractsWithIdenticalOZBlock)),
+                'Percentage ratio: {}%.'.format(round((len(uniqueContractsWithIdenticalOZBlock)/len(unique_contracts))*100, 2))
+            )
+
+            reports.append(report)
+        
+        self.printTextReport('12b', reports)
         
     def observation13(self):
         df = pd.read_csv('../03_clones/rq3/type-1.csv') 
-        all_corpus_contracts = pd.read_csv('../03_clones/rq3/corpus_contracts.csv')
+
+        all_corpus_contracts = pd.read_csv('../03_clones/rq3/corpus_functions.csv')
 
         openZeppelinRecords = (df[df['filename_y'].notnull()]).drop_duplicates(subset=['filename_x', 'startline_x', 'endline_x'])
         
@@ -667,9 +779,36 @@ class Analysis():
         
         self.printTextReport(13, report)
 
+    def observation13b(self):
+        df = pd.read_csv('../03_clones/rq3/type-1_functions.csv') 
+        df['endline_x'] = df['endline_x'].apply( lambda x: int(x.replace('"', '')))
+        df['startline_x'] = df['startline_x'].apply( lambda x: int(x.replace('"', '')))
+        df['diff'] = df['endline_x'] - df['startline_x'] + 1
+
+        all_corpus_contracts = pd.read_csv('../03_clones/rq3/corpus_functions.csv')
+        all_corpus_contracts['endline'] = all_corpus_contracts['endline'].apply( lambda x: int(x.replace('"', '')))
+        all_corpus_contracts['startline'] = all_corpus_contracts['startline'].apply( lambda x: int(x.replace('"', '')))
+        all_corpus_contracts['diff'] = all_corpus_contracts['endline'] - all_corpus_contracts['startline'] + 1
+
+        reports = []
+        for i in range(11):
+            df = df[df['diff'] >= i]
+
+            all_corpus_contracts = all_corpus_contracts[all_corpus_contracts['diff'] >= i]
+            openZeppelinRecords = (df[df['filename_y'].notnull()]).drop_duplicates(subset=['filename_x', 'startline_x', 'endline_x'])
+
+            report = (f'Min number of lines considered: {i}', 
+                'Total number of code blocks: {}.'.format(len(all_corpus_contracts)),
+                'Number of distinct OpenZeppelin records: {}.'.format(len(openZeppelinRecords)),
+                'Percentage ratio: {}%.'.format(round((len(openZeppelinRecords)/len(all_corpus_contracts))*100, 2)))
+           
+            reports.append(report)
+        
+        self.printTextReport('13b', reports)
 
     def observation14(self):
         df = pd.read_csv('../03_clones/rq3/type-1.csv') 
+
         ozFiles = df.filename_y.apply(lambda x: x.replace('"','').split('/')[-1] if not pd.isna(x) else x)
         
         totalOZFiles = ozFiles.value_counts().sum()
@@ -686,27 +825,82 @@ class Analysis():
             ('Cumulative 80%', ozCounts.head(ozCounts[ozCounts.cumulativePerc > 80].index[0]), ''),
             ('First 20', ozCounts.head(int(len(ozCounts)*0.2)), '')
         ]
+
+        print(ozCounts.head(10).to_latex())
         
         self.printHtmlReport(14, report)
 
+    def observation14b(self):
+        df = pd.read_csv('../03_clones/rq3/type-1_functions.csv') 
+
+        df['endline_x'] = df['endline_x'].apply( lambda x: int(x.replace('"', '')))
+        df['startline_x'] = df['startline_x'].apply( lambda x: int(x.replace('"', '')))
+        df['diff'] = df['endline_x'] - df['startline_x'] + 1
+
+        reports = []
+
+        from collections import namedtuple
+        report_with_cutoff = namedtuple('REPORT_WITH_CUTOFF', ('min_lines', 'report'))
+
+        for i in range(11):
+            df = df[df['diff'] >= i]
+
+            ozFiles = df.function_id_y
+            
+            totalOZFiles = ozFiles.value_counts().sum()
+            uniqueOZFileNames = len(ozFiles.unique())
+            
+            ozCounts = ozFiles.value_counts().rename_axis('functions').reset_index(name='count')
+            
+            ozCounts['cumulativeSum'] = ozCounts['count'].cumsum()
+            ozCounts['perc'] = round((ozCounts['count']/totalOZFiles)*100, 2)
+            ozCounts['cumulativePerc'] = round((ozCounts['cumulativeSum']/totalOZFiles)*100, 2)
+
+            report = [
+                ('Head 10', ozCounts.head(10), ''),
+                ('Cumulative 80%', ozCounts.head(ozCounts[ozCounts.cumulativePerc > 80].index[0]), ''),
+                ('First 20', ozCounts.head(int(len(ozCounts)*0.2)), '')
+            ]
+
+            reports.append(report_with_cutoff(i, report))
+        
+        self.printHtmlReport('14b', reports, functions=True)
     
     ########################## HELPER METHODS ##########################            
     def savefig(self, observationNumber):
         plt.gcf().tight_layout()
         plt.savefig('{}/observation{}.pdf'.format(resultsPath, observationNumber))
             
-    def printTextReport(self, observationNumber, reports):
+    def printTextReport(self, observationNumber, reports:list):
         f = open('{}/observation{}.txt'.format(resultsPath, observationNumber), 'w')
-        for r in reports:
-            f.write(r+'\n')
+        reports = pformat(reports)
+        f.write(reports)
         f.close()
         
-    def printHtmlReport(self, observationNumber, reports):
+    def printHtmlReport(self, observationNumber, reports:list, functions=False):
         f = open('{}/observation{}.html'.format(resultsPath, observationNumber), 'w')
-        for title, df, comment in reports:
-            f.write('<h2>{}</h2>'.format(title))
-            f.write(df.to_html())
-            f.write('<p>{}</p>'.format(comment))
+        if functions:
+            for cutoff_report in reports:
+                cutoff = cutoff_report.min_lines
+                report = cutoff_report.report
+                f.write(f'<h1> Min_lines: {cutoff}</h1>')
+                f.write('<div style="display:flex">')
+                for title, df, comment in report:
+                    f.write('<div style="margin-right:20px">')
+                    f.write('<h2>{}</h2>'.format(title))
+                    f.write(df.to_html())
+                    f.write('<p>{}</p>'.format(comment))
+                    f.write('</div>')
+                f.write('</div>')
+        else:
+            f.write('<div style="display:flex">')
+            for title, df, comment in reports:
+                f.write('<div style="margin-right:20px">')
+                f.write('<h2>{}</h2>'.format(title))
+                f.write(df.to_html())
+                f.write('<p>{}</p>'.format(comment))
+                f.write('</div>')
+            f.write('</div>')
         f.close()
 
     def runAll(self):
